9a27ac5009f5570c2fbbc21968f3494bf1133e4b
Kushal <khandelwal.kushal@gmail.com>
2020-03-30 13:33:38 -0700

    Update sharedworkers.js

--- sharedworkers.js
+++ sharedworkers.js
@@ -1,9 +1,20 @@
-var a = 1;
+var time = 0,
+    intervalId;
 
 onconnect = function (e) {
     var port = e.ports[0];
 
-    port.onmessage = function () {
-        port.postMessage(a++)
+    port.onmessage = function (event) {
+        if(event.data === 'start'){
+            intervalId = setInterval(sendTime, 1000)
+        }else{
+            clearInterval(intervalId);
+            time = 0;
+        }
+        
+    }
+    
+    function sendTime(){
+        port.postMessage(time++);
     }
 }
